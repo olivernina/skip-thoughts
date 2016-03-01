e42b4330b1ff536104c1bccba6558583e9aed221
ryan <pk_blaze@knights.ucf.edu>
2016-02-29 20:04:38 -0500

    skip

--- skipthoughts.py
+++ skipthoughts.py
@@ -20,8 +20,8 @@ profile = False
 #-----------------------------------------------------------------------------#
 # Specify model and table locations here
 #-----------------------------------------------------------------------------#
-path_to_models = '/u/rkiros/public_html/models/'
-path_to_tables = '/u/rkiros/public_html/models/'
+path_to_models = './skip-thoughts/data/'
+path_to_tables = './skip-thoughts/data/'
 #-----------------------------------------------------------------------------#
 
 path_to_umodel = path_to_models + 'uni_skip.npz'
